41bdf219868b8497b4147eb283b8672f41fa0bd1
PrasadBQA <prasadbaburi.space@gmail.com>
2026-08-26 09:43:02 +0530

    Add async, module, and class examples

---
+++ 19_Chapter_Export_Import/155_Import.js
@@ -0,0 +1,7 @@
+import { BASE_URL, formatUpperCaseString } from "./Export_testutils.js";
+
+console.log(BASE_URL);
+
+let result = formatUpperCaseString("Hello");
+console.log(result);
+
---
+++ 19_Chapter_Export_Import/156_Import.js
@@ -0,0 +1,6 @@
+import { BASE_URL as base_url2, formatTestName} from "./Export_utils.js";
+import { BASE_URL as base_url3, formatUpperCaseString } from "./Export_testutils.js";
+
+console.log(base_url2);
+console.log(base_url3);
+console.log(formatTestName("Hello"));
